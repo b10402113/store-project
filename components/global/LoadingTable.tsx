9403c8c16f8c7dec8cf064afdddfde46130aa3e0
Andy Cheng <b10402113@gmail.com>
2026-01-27 07:47:31 +0800

    修正 LoadingTable 預設行數為 5

--- components/global/LoadingTable.tsx
+++ components/global/LoadingTable.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Skeleton } from "../ui/skeleton";
 
-export default function LoadingTable({ rows }: { rows?: number }) {
+export default function LoadingTable({ rows = 5 }: { rows?: number }) {
   const tableRows = Array.from({ length: rows }, (_, index) => {
     return (
       <div className="mb-4" key={index}>
